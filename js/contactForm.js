38a46b77606c8aeba0a05707dfc0e0e9ec0542e0
Jesse Daniels <info@jesseandgena.com>
2020-07-28 17:31:25 -0400

    adds notes to js file

--- js/contactForm.js
+++ js/contactForm.js
@@ -3,14 +3,15 @@ function validateForm() {
     var lname = document.forms["contactForm"]["lname"].value;
     var validate = document.forms["contactForm"]["validate"].value;
 
+
     if (fname == null || fname == "") {
-        alert("First name must be filled out");
+        alert("First name must be filled out"); // alert user if first name field is missing 
         return false;
-    } else if (lname == null || lname == "") {
-        alert("Last name must be filled out");
+    } else if (lname == null || lname == "") { 
+        alert("Last name must be filled out"); // alert user if last name field is missing 
         return false;
-    } else if (validate != 4) {
-        alert("Maybe math is not your strong suit? Please try again.");
+    } else if (validate != 4) { 
+        alert("Maybe math is not your strong suit? Please try again."); // if user gives any answer other than 4 they give an alert message
         return false;
     }
 }
